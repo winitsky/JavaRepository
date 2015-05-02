5a8a6617b75d316c37ace5d142b07e85cafcc05e
winitsky <winitsky@mail.ru>
2015-05-02 21:18:53 +0300

    Задание №3 - определение является ли полседняя цифра числа семеркой

---
+++ AlexWinitsky/src/lab3/Seven.java
@@ -0,0 +1,16 @@
+package lab3;
+
+/*Задание №3 - определение является ли полседняя цифра числа семеркой
+ * Created by Master on 24.04.2015.
+ */
+public class Seven {
+    public static void main(String[] args) {
+        int number = 277;
+        if ((number % 10) == 7) {
+            System.out.println("полседняя цифра числа " + number + " семь");
+        } else {
+            System.out.println("полседняя цифра числа " + number + " не семь");
+        }
+
+    }
+}
