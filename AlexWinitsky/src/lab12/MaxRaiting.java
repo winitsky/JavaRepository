2e03bbfcf130cc51154bd49f4a6e3002d3221f3d
winitsky <winitsky@mail.ru>
2015-05-04 08:43:14 +0300

    Задание 12   В задаче на поиск максимальной оценки, вывести не саму оценку, а ее номер.

---
+++ AlexWinitsky/src/lab12/MaxRaiting.java
@@ -0,0 +1,21 @@
+package lab12;
+
+/**
+ * Задание 12
+ * В задаче на поиск максимальной оценки, вывести не саму оценку, а ее номер.
+ */
+public class MaxRaiting {
+    public static void main(String[] args) {
+        int raiting[] = {7, 8, 9, 10, 5, 6, 8, 5, 3, 4, 5, 6, 8, 7};
+        int maxid = 0;
+        int max = raiting[0];
+        for (int i = 0; i < raiting.length; i++) {
+            if (max < raiting[i]) {
+                max = raiting[i];
+                maxid = i;
+            }
+        }
+        System.out.println("мaксимальную оценку  " + max + " имеет элемент с индексом " + maxid);
+    }
+}
+
