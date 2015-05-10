60a40da30b1b28e2ec6595093e2fb350b2fd6cc5
winitsky <winitsky@mail.ru>
2015-05-10 20:01:42 +0300

    Задание 6  Определить число, полученное выписыванием в обратном порядке цифр любого натурального числа n.(исправленное)

--- AlexWinitsky/src/lab6/ReversNumber.java
+++ AlexWinitsky/src/lab6/ReversNumber.java
@@ -7,15 +7,15 @@ package lab6;
 public class ReversNumber {
     public static void main(String[] args) {
         int number = 34506741;
-        int reversnumber = 0;
+        int reversNumber = 0;
 
         System.out.println("Исходное число " + number);
         while (number > 0) {
-            reversnumber *= 10;
-            reversnumber += number % 10;
+            reversNumber *= 10;
+            reversNumber += number % 10;
             number = number / 10;
         }
 
-        System.out.println("число, полученное выписыванием в обратном порядке цифр " + reversnumber);
+        System.out.println("число, полученное выписыванием в обратном порядке цифр " + reversNumber);
     }
 }
