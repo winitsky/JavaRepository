6a3562ae987c5aa241372ff753a6126e01d8bd0d
winitsky <winitsky@mail.ru>
2015-05-11 15:06:11 +0300

    *Задание 21  * Написать программу, выполняющую поиск в строке всех тегов абзацев,  * в т.ч. тех, у которых есть параметры, например <p id=”p1”>, и замену их на простые теги абзацев <p>.

---
+++ AlexWinitsky/src/lab21/FindTegP.java
@@ -0,0 +1,25 @@
+package lab21;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Задание 21
+ * Написать программу, выполняющую поиск в строке всех тегов абзацев,
+ * в т.ч. тех, у которых есть параметры, например <p id=”p1”>, и замену их на простые теги абзацев <p>.
+ */
+public class FindTegP {
+
+    private final String REGULAR_EXPRESSION = "<p\\s.*?>";
+
+    String findTeg(String str) {
+        Pattern pattern = Pattern.compile(REGULAR_EXPRESSION);
+        Matcher matcher = pattern.matcher(str);
+        while (matcher.find()){
+            System.out.println(matcher.start()+" "+matcher.end());
+        }
+        matcher.reset();
+        return matcher.replaceAll("<p>");
+    }
+
+}
---
+++ AlexWinitsky/src/lab21/RunFindTegP.java
@@ -0,0 +1,15 @@
+package lab21;
+
+/**
+ *Задание 21
+ * Написать программу, выполняющую поиск в строке всех тегов абзацев,
+ * в т.ч. тех, у которых есть параметры, например <p id=”p1”>, и замену их на простые теги абзацев <p>.
+ */
+public class RunFindTegP {
+    public static void main(String[] args) {
+        String str = " Написать программу <p   id=p1> выполняющую поиск в строке всех тегов <p class=main>  на простые теги абзацев <p> ";
+        System.out.println("Исходная строка "+str);
+        FindTegP findTegP=new FindTegP();
+        System.out.println(findTegP.findTeg(str));
+    }
+}
