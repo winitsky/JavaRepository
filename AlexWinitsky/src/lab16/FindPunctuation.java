d885a94859e327bce1fce4129d9821e9f8330f0e
winitsky <winitsky@mail.ru>
2015-05-15 15:39:19 +0300

    * Задание 16  * Найти в строке не только запятые, но и другие знаки препинания.  С исправлениями.

--- AlexWinitsky/src/lab16/FindPunctuation.java
+++ AlexWinitsky/src/lab16/FindPunctuation.java
@@ -5,21 +5,15 @@ package lab16;
  * Найти в строке не только запятые, но и другие знаки препинания. Подсчитать общее их количество.
  */
 public class FindPunctuation {
-    private String str;
-
-    FindPunctuation(String str) {
-        this.str = str;
-    }
-
-    public int find() {
-        int n = 0;
+    public int find(String str) {
+        int number = 0;
         for (int i = 0; i < str.length(); i++) {
             char symbol = str.charAt(i);
             if (symbol == ',' || symbol == ':' || symbol == '-' || symbol == '.' || symbol == '-') {
-                n++;
+                number++;
             }
         }
-        return n;
+        return number;
     }
 
 
--- AlexWinitsky/src/lab16/TestPunctuation.java
+++ AlexWinitsky/src/lab16/TestPunctuation.java
@@ -7,7 +7,7 @@ package lab16;
 public class TestPunctuation {
     public static void main(String[] args) {
         String str = "Для месяцев и дней недели в классе описаны константы: JANUARY, FEBRUARY, WEDNESDAY, THURSDAY, FRIDAY,  SATURDAY ";
-        FindPunctuation punctuation = new FindPunctuation(str);
-        System.out.println("Количесвто знаков припинания в строке " + punctuation.find());
+        FindPunctuation punctuation = new FindPunctuation();
+        System.out.println("Количесвто знаков припинания в строке " + punctuation.find(str));
     }
 }
