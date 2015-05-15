ca4ec8ed4137fedbfcbe80a50fbc2cbeac5520bd
winitsky <winitsky@mail.ru>
2015-05-15 16:18:32 +0300

    Задание 18  * Имеется строка с текстом. Вывести текст, составленный из послед-них букв всех слов. С исправлениями.

--- AlexWinitsky/src/lab18/LastLetters.java
+++ AlexWinitsky/src/lab18/LastLetters.java
@@ -4,27 +4,20 @@ package lab18;
  * Created by Master on 08.05.2015.
  */
 public class LastLetters {
-    private String str;
 
-    LastLetters(String str) {
-        this.str = str;
-    }
-
-    String findLastLetters() {
-        String newstr = "";
-        char symbol;
+    public String findLastLetters(String str) {
+        String newStr = "";
         str = str.trim();
         str = str.replaceAll(" +", " ");
         for (int i = 0; i < str.length(); i++) {
-            symbol = str.charAt(i);
+            char symbol = str.charAt(i);
             if (symbol == ' ' || symbol == '.' || symbol == ',' || symbol == '-' || symbol == ':') {
-                if (str.charAt(i - 1) == ',' || str.charAt(i - 1) == '.' || str.charAt(i - 1) == ':' || str.charAt(i - 1) == '-') {
-                } else {
-                    newstr += str.charAt(i - 1);
+                if (str.charAt(i - 1) != ',' && str.charAt(i - 1) != '.' && str.charAt(i - 1) != ':' && str.charAt(i - 1) != '-') {
+                    newStr += str.charAt(i - 1);
                 }
             }
         }
-        return newstr;
+        return newStr;
     }
 
 }
