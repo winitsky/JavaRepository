2a1e96a506f7fb268cffc20548ac3ae5c8a6e234
winitsky <winitsky@mail.ru>
2015-05-17 12:34:52 +0300

    Доп. задание 10.Взять любой русскоязычный̆ текст, в котором не меньше трёхсот знаков. Необходимо провести анализ этого текста.

---
+++ AlexWinitsky/src/addtask10/TextFunction.java
@@ -0,0 +1,60 @@
+package addtask10;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * 7.Взять любой русскоязычный̆ текст, в котором не меньше трёхсот знаков.
+ * Необходимо провести анализ этого текста на:
+ * a)	   Количество согласных и гласных букв.
+ * b)	   Вывести каждое предложение этого текста в отдельной̆ строке с указанием количества символов в нём.
+ * c)	   Посчитать количество пробелов и абзацев (абзацем считается переход на новую строку).
+ * d)	   Создать текст, в котором будут убраны все пробельные символы в исходном тексте, и вывести его.
+ */
+public class TextFunction {
+
+    public int numbersOfVowel(String text) {
+        Pattern vowelLetter = Pattern.compile("[аеёиоуыэюяАЕЁИОУЫЭЮЯ]");
+        Matcher matcherVowel = vowelLetter.matcher(text);
+
+        int number = 0;
+        while (matcherVowel.find()) {
+            number++;
+        }
+        return number;
+    }
+
+    public int numberOfConsonants(String text) {
+        Pattern consonants = Pattern.compile("[^аеёиоуыэюя]");
+        Matcher matcherConsonants = consonants.matcher(text);
+        int number = 0;
+        while (matcherConsonants.find()) {
+            number++;
+        }
+        return number;
+    }
+
+    public int numberOfGap(String text) {
+        Pattern patternGap = Pattern.compile("[\\s]");
+        Matcher matcherGap = patternGap.matcher(text);
+        int number = 0;
+        while (matcherGap.find()) {
+            number++;
+        }
+        return number;
+    }
+
+    public String printSentence(String text) {
+        String newText = "";
+        String array[] = (text.replaceAll("\\n+", "")).split("\\.+");
+        for (int i = 0; i < array.length; i++) {
+            newText += array[i].trim() + "." + " - Количество символов " + array[i].length() + "\n";
+        }
+        return newText;
+    }
+
+    public String printTextWithoutGap(String text) {
+        return text.replaceAll(" +", "");
+    }
+
+}
