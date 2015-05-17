c23ea3296dbae766489fe1ff4084993a8a0b194d
winitsky <winitsky@mail.ru>
2015-05-17 12:40:01 +0300

    Доп. задание.  Заменить в строке все вхождения 'child' на 'children'.  Удалить из текста все символы, являющиеся цифрами.

---
+++ AlexWinitsky/src/addtask11/RunTextFunction.java
@@ -0,0 +1,17 @@
+package addtask11;
+
+/**
+ * Заменить в строке все вхождения 'child' на 'children'.  Удалить из текста все символы, являющиеся цифрами.
+ */
+public class RunTextFunction {
+    public static void main(String[] args) {
+        String text="Our child is 5 years. My brothers child is 4 years. My siters children is 7 years. ";
+        TextFunction textFunction=new TextFunction();
+        System.out.println("Исходное предложение");
+        System.out.println(text);
+        System.out.println("Удалить из текста все символы, являющиеся цифрами.");
+        textFunction.deleteNumbers(text);
+        System.out.println("Заменить в строке все вхождения 'child' на 'children'");
+        textFunction.FindAndReplaceWord(text);
+    }
+}
---
+++ AlexWinitsky/src/addtask11/TextFunction.java
@@ -0,0 +1,21 @@
+package addtask11;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Заменить в строке все вхождения 'child' на 'children'.  Удалить из текста все символы, являющиеся цифрами.
+ */
+public class TextFunction {
+    public void deleteNumbers(String text) {
+        Pattern pattern = Pattern.compile("\\d");
+        Matcher matcher = pattern.matcher(text);
+        System.out.println(matcher.replaceAll(""));
+    }
+
+    public void FindAndReplaceWord(String text) {
+        Pattern pattern = Pattern.compile("child");
+        Matcher matcher = pattern.matcher(text);
+        System.out.println(matcher.replaceAll("children"));
+    }
+}
