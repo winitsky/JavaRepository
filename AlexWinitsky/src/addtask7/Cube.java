d0857614301c8b0db0901293ded5ec9db24a3ec6
winitsky <winitsky@mail.ru>
2015-05-18 12:13:01 +0300

    Доп.задание №7. Описать класс куб. С исправлениями.

--- AlexWinitsky/src/addtask7/Cube.java
+++ AlexWinitsky/src/addtask7/Cube.java
@@ -4,25 +4,25 @@ package addtask7;
  * Created by Master on 04.05.2015.
  */
 public class Cube {
-    int side;
-    String color;
+    private int side;
+    private String color;
 
-    Cube(int side, String color){
-        this.side=side;
-        this.color=color;
-        System.out.println("Создан объект куб, с стороной = " + this.side + " и цветом =  "+ this.color );
+    public Cube(int side, String color) {
+        this.side = side;
+        this.color = color;
+        System.out.println("Создан объект куб, с стороной = " + this.side + " и цветом =  " + this.color);
     }
 
-    public int squareCube(){
-        return 6*this.side*this.side;
+    public int cubeArea() {
+        return 6 * side * side;
     }
 
-    public int volumeCube(){
-        return this.side*this.side*this.side;
+    public int cubeVolume() {
+        return side * side * side;
     }
 
-    void colorCube(String newColor){
-        this.color=newColor;
-        System.out.println("Установлен новый цвет куба "+this.color);
+    public void cubeColor(String newColor) {
+        color = newColor;
+        System.out.println("Установлен новый цвет куба " + color);
     }
 }
