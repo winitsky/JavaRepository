447e92e6e9ce225199afbc70150e303ab0e09506
winitsky <winitsky@mail.ru>
2015-06-12 12:06:52 +0300

    * Задание 32 * Записать в двоичный файл 20 случайных чисел. С исправлениями.

--- AlexWinitsky/src/lab32/BinaryFile.java
+++ AlexWinitsky/src/lab32/BinaryFile.java
@@ -1,12 +1,13 @@
 package lab32;
 
 import java.io.*;
+import java.util.Arrays;
 
 
 public class BinaryFile {
     public void writeFile() {
 
-        try (DataOutputStream dataOutputStream = new DataOutputStream(new FileOutputStream("./src/lab32/Number.dat"))) {
+        try (DataOutputStream dataOutputStream = new DataOutputStream(new BufferedOutputStream(new FileOutputStream("./AlexWinitsky/src/lab32/Number.dat")))) {
             for (int i = 0; i < 20; i++) {
                 dataOutputStream.writeInt((int) (Math.random() * 100));
             }
@@ -17,18 +18,29 @@ public class BinaryFile {
         }
     }
 
-    public void readFile() {
-
-        try (DataInputStream dataInputStream = new DataInputStream(new FileInputStream("./src/lab32/Number.dat"))) {
-            int number = dataInputStream.readInt();
+    public int[] readFile() {
+        int array[] = new int[20];
+        try (DataInputStream dataInputStream = new DataInputStream(new FileInputStream("./AlexWinitsky/src/lab32/Number.dat"))) {
             for (int i = 0; i < 19; i++) {
-                System.out.println(number);
-                number = dataInputStream.readInt();
+                array[i] = dataInputStream.readInt();
             }
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
+        return array;
+    }
+
+    public int arithmeticMean(int[] array) {
+        int sum = 0;
+        for (int i = 0; i < array.length; i++) {
+            sum = sum + array[i];
+        }
+        return sum / array.length;
+    }
+
+    public void printNumber(int array[]) {
+        System.out.println(Arrays.toString(array));
     }
 }
--- AlexWinitsky/src/lab32/RunNumberInBinaryFile.java
+++ AlexWinitsky/src/lab32/RunNumberInBinaryFile.java
@@ -1,14 +1,18 @@
 package lab32;
 
+import com.sun.org.apache.xpath.internal.SourceTree;
+
 /**
- Задание 32
- Записать в двоичный файл 20 случайных чисел.
- Прочитать запи-санный файл, распечатать числа и их среднее арифметическое.
+ * Задание 32
+ * Записать в двоичный файл 20 случайных чисел.
+ * Прочитать запи-санный файл, распечатать числа и их среднее арифметическое.
  */
 public class RunNumberInBinaryFile {
     public static void main(String[] args) {
-        BinaryFile binaryFile=new BinaryFile();
+        BinaryFile binaryFile = new BinaryFile();
         binaryFile.writeFile();
-        binaryFile.readFile();
+        System.out.println("Чила из двоичного файла");
+        binaryFile.printNumber(binaryFile.readFile());
+        System.out.println("Среднеарифметическое число " + binaryFile.arithmeticMean(binaryFile.readFile()));
     }
 }
